035fb4bee6f9911008248c130fc05eabd31e1408
jsk900 <jsk900@yahoo.co.uk>
2022-01-19 14:11:58 +0100

    nested routes

---
+++ src/components/pages/Selections.jsx
@@ -0,0 +1,26 @@
+import { Link, Outlet } from 'react-router-dom';
+
+const Selections = ({ data }) => {
+  const { results, loading, error } = data;
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  const get30 = () => results.filter((result) => result.title.length <= 30);
+  const get100 = () => results.filter((result) => result.title.length > 30);
+
+  return (
+    <section>
+      <h1>Nested Routes</h1>
+      <Link to='titles30' state={get30()}>
+        Titles 30
+      </Link>
+      <Link to='titles100' state={get100()}>
+        Titles 100
+      </Link>
+      <Outlet />
+    </section>
+  );
+};
+
+export default Selections;
